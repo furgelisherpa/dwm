4644852c40facc14cb60373a0b66a5adaa2bca9e
furgelisherpa <furgelisherpa@proton.me>
2023-01-04 15:31:03 +0545

    use joypixels only

--- config.h
+++ config.h
@@ -18,7 +18,6 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = {"monospace:size=11:antialias=true:hinting=true",
                                        "JoyPixels:pixelsize=12:antialias=true:autohint=true",
-                                       "NotoColorEmoji:size=12:antialias=true:autohint=true",
                                        "SymbolsNerdFont:size=11:antialias=true:autohint=true"
 						     	};
 static char normbgcolor[]           = "#222222";
